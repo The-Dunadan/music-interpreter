9f3b46b51c265b46edac05c9a23f882b1b561885
The-Dunadan <stefan.vodita@gmail.com>
2019-02-11 12:39:32 +0200

    Add run rule to Makefile

--- main.c
+++ main.c
@@ -3,5 +3,12 @@
 
 
 int main() {
-	int status = system("paplay sound-library/out.ogg");
+	/*system("paplay sound-library/out.ogg");
+	system("paplay sound-library/out1.ogg");
+	system("paplay sound-library/out2.ogg");*/
+
+	system("paplay a1.ogg");
+	system("paplay a1.ogg");
+	system("paplay a1.ogg");
+	system("paplay a1.ogg");
 }
