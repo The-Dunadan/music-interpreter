41bdc923a2d4b7d0efd8c8147f099f6bf4755af0
The-Dunadan <stefan.vodita@gmail.com>
2019-02-11 13:04:49 +0200

    Sing the first song

--- main.c
+++ main.c
@@ -3,12 +3,23 @@
 
 
 int main() {
-	system("paplay sound-library/cn4-1.ogg");
-	system("paplay sound-library/dn4-1.ogg");
 	system("paplay sound-library/en4-1.ogg");
-	system("paplay sound-library/fn4-1.ogg");
-	system("paplay sound-library/gn4-1.ogg");
 	system("paplay sound-library/an5-1.ogg");
-	system("paplay sound-library/bn5-1.ogg");
+	system("paplay sound-library/en5-1.ogg");
 	system("paplay sound-library/cn5-1.ogg");
+
+	system("paplay sound-library/dn4-1.ogg");
+	system("paplay sound-library/an5-1.ogg");
+	system("paplay sound-library/fn5-1.ogg");
+	system("paplay sound-library/dn5-1.ogg");
+
+	system("paplay sound-library/dn4-1.ogg");
+	system("paplay sound-library/gn4-1.ogg");
+	system("paplay sound-library/gn5-1.ogg");
+	system("paplay sound-library/bn5-1.ogg");
+
+	system("paplay sound-library/dn4-1.ogg");
+	system("paplay sound-library/gn4-1.ogg");
+	system("paplay sound-library/gn5-1.ogg");
+	system("paplay sound-library/gn5-1.ogg");
 }
