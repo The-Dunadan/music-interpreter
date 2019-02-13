b04c34d85fc1fcf731a54986fbb890ada84b70e8
The-Dunadan <stefan.vodita@gmail.com>
2019-02-13 11:54:41 +0200

    Play chords

--- main.py
+++ main.py
@@ -1,13 +1,35 @@
 import subprocess
+import re
+import time
 
 player = "paplay"
 soundLibraryPath = "sound-library/"
 audioFileType = ".ogg"
 
 
-tmnFile = open("songs/first.tmn", "r")
-notes = tmnFile.read().replace('\n', '').split('/')
-print(notes)
+tmnFile = open("songs/hobbit's song.tmn", "r")
+chords = tmnFile.read().replace('\n', '').split('/')
+print(chords)
 
-for note in notes:
-	subprocess.run([player, soundLibraryPath + note + audioFileType])
+durations = [0] * len(chords)
+for i in range(len(chords)):
+	chord = chords[i]
+	print(chord)
+	for j in range(len(chord)):
+		if chord[j] == '-':
+			duration = chord[j + 1:]
+			duration = duration[0 : (duration.find('|') if duration.find('|') != -1 
+				                                        else len(duration))]
+			#print(int(duration))
+			if int(duration) > durations[i]:
+				durations[i] = int(duration)
+
+#print(durations)
+
+for i in range(len(chords)):
+	chord = chords[i]
+	print(chord)
+	notes = chord.split('|')
+	for note in notes:
+		subprocess.Popen([player, soundLibraryPath + note + audioFileType])
+	time.sleep(4/durations[i])
